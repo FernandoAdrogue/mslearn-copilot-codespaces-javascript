6e617e81c71a7cbabd4fd513a81ba73c302309b7
Chino! <121034778+FernandoAdrogue@users.noreply.github.com>
2023-11-01 15:18:14 +0000

    Add personal data ando syle to social icons

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Fernando Marcel Adrogué Benas",
+  title: "Full Stack Developer",
+  email: "chinoadrogue@gmail.com",
+  gitHub: "https://github.com/FernandoAdrogue",
+  instagram: "",
+  linkedIn: "https://www.linkedin.com/in/fernando-marcel-adrogu%C3%A9-benas/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
